b7835b44da485726f0dc090a3380650bba3abcf2
tony un <tony un>
2016-06-28 22:37:36 -0400

    Commit for bob, 17/20 pass.

--- bob.rb
+++ bob.rb
@@ -1,2 +1,15 @@
 class Bob
+  def hey(remark)
+    if (remark == remark.downcase.upcase) && remark.length>7
+      'Whoa, chill out!'
+    elsif remark[-1] == "?"
+      'Sure.'
+    elsif remark[-1] == "." || "!"
+       'Whatever.'
+    elsif remark ==
+      'Fine. Be that way!'
+    else
+    end
+
+  end
 end
